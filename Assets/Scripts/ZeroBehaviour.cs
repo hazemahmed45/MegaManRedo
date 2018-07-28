441296be4aec6bc7b3be36b238069122c943c0ee
hazemahmed <hazemahmed45@gmail.com>
2018-07-29 01:05:30 +0200

    assets added

--- Assets/Scripts/ZeroBehaviour.cs
+++ Assets/Scripts/ZeroBehaviour.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ZBehaviour : MonoBehaviour,IPlayerBehaviour,IPlayerAnimation,IPlayerSounds {
+public class ZeroBehaviour : MonoBehaviour,IPlayerAnimation,IPlayerBehaviour,IPlayerSounds {
     public void dash()
     {
         throw new System.NotImplementedException();
